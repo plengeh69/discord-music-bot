122c56086fe704c0f1873a0daa06cfa9512020ee
mamasu <63069639+plengeh69@users.noreply.github.com>
2020-10-06 16:41:23 +0700

    Update message.js

--- events/message.js
+++ events/message.js
@@ -1,27 +1,27 @@
 module.exports = async (client, message) => {
-    if (message.author.bot) return;
+  if (message.author.bot) return;
 
+  //Prefixes also have mention match
+  const prefixMention = new RegExp(`^<@!?${client.user.id}> `);
+  const prefix = message.content.match(prefixMention) ? message.content.match(prefixMention)[0] : client.config.prefix;
 
-    const prefixMention = new RegExp(`^<@!?${client.user.id}> `);
-    const prefix = message.content.match(prefixMention) ? message.content.match(prefixMention)[0] : client.config.prefix;
+  if (message.content.indexOf(prefix) !== 0) return;
 
-    if (message.content.indexOf(prefix) !== 0) return;
+  const args = message.content.slice(prefix.length).trim().split(/ +/g);
+  //Making the command lowerCase because our file name will be in lowerCase
+  const command = args.shift().toLowerCase();
 
-    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+  //Searching a command
+  const cmd = client.commands.get(command);
+  //Searching a command aliases
+  const aliases = client.commands.find(x => x.info.aliases.includes(command))
 
-    const command = args.shift().toLowerCase();
+  //if(message.channel.type === "dm")return message.channel.send("None of the commands work in DMs. So please use commands in server!")
 
-
-    const cmd = client.commands.get(command);
-
-    const aliases = client.commands.find(x => x.info.aliases.includes(command))
-
-
-
-
-    if(cmd){
-        cmd.run(client, message, args);
-    }else if(aliases){
-        aliases.run(client, message, args);
-    }else return
+  //Executing the codes when we get the command or aliases
+  if(cmd){
+    cmd.run(client, message, args);
+  }else if(aliases){
+    aliases.run(client, message, args);
+  }else return
 };
